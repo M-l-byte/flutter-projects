a6ff67d8d0fdc0adad0f2f2e4e1c646ddceb37f2
Mohit <mohitkanwal2019@gmail.com>
2023-08-22 09:27:50 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           title: const Text(
             "this is the app2",
             style: TextStyle(
-              color: Colors.deepPurpleAccent,
+              color: Colors.blueAccent,
               fontFamily: 'ftr',
             ),
           ),
